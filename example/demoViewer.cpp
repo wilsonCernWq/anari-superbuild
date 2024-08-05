682ee1e988def32bfd1027e063ee84b320dd2c61
Qi Wu <wilson.over.cloud@gmail.com>
2024-08-04 18:34:27 -0700

    enable viewer

--- example/demoViewer.cpp
+++ example/demoViewer.cpp
@@ -17,8 +17,289 @@ static std::string g_libraryName = "environment";
 static anari::Library g_debug = nullptr;
 static anari::Device g_device = nullptr;
 static const char *g_traceDir = nullptr;
+extern const char *g_defaultLayout;
 
-// extern const char *getDefaultUILayout();
+namespace anari {
+namespace scenes {
+
+// quad mesh data
+static std::vector<math::float3> vertices = {
+    // Floor
+    {1.00f, -1.00f, -1.00f},
+    {-1.00f, -1.00f, -1.00f},
+    {-1.00f, -1.00f, 1.00f},
+    {1.00f, -1.00f, 1.00f},
+    // Ceiling
+    {1.00f, 1.00f, -1.00f},
+    {1.00f, 1.00f, 1.00f},
+    {-1.00f, 1.00f, 1.00f},
+    {-1.00f, 1.00f, -1.00f},
+    // Backwall
+    {1.00f, -1.00f, 1.00f},
+    {-1.00f, -1.00f, 1.00f},
+    {-1.00f, 1.00f, 1.00f},
+    {1.00f, 1.00f, 1.00f},
+    // RightWall
+    {-1.00f, -1.00f, 1.00f},
+    {-1.00f, -1.00f, -1.00f},
+    {-1.00f, 1.00f, -1.00f},
+    {-1.00f, 1.00f, 1.00f},
+    // LeftWall
+    {1.00f, -1.00f, -1.00f},
+    {1.00f, -1.00f, 1.00f},
+    {1.00f, 1.00f, 1.00f},
+    {1.00f, 1.00f, -1.00f},
+    // ShortBox Top Face
+    {-0.53f, -0.40f, -0.75f},
+    {-0.70f, -0.40f, -0.17f},
+    {-0.13f, -0.40f, -0.00f},
+    {0.05f, -0.40f, -0.57f},
+    // ShortBox Left Face
+    {0.05f, -1.00f, -0.57f},
+    {0.05f, -0.40f, -0.57f},
+    {-0.13f, -0.40f, -0.00f},
+    {-0.13f, -1.00f, -0.00f},
+    // ShortBox Front Face
+    {-0.53f, -1.00f, -0.75f},
+    {-0.53f, -0.40f, -0.75f},
+    {0.05f, -0.40f, -0.57f},
+    {0.05f, -1.00f, -0.57f},
+    // ShortBox Right Face
+    {-0.70f, -1.00f, -0.17f},
+    {-0.70f, -0.40f, -0.17f},
+    {-0.53f, -0.40f, -0.75f},
+    {-0.53f, -1.00f, -0.75f},
+    // ShortBox Back Face
+    {-0.13f, -1.00f, -0.00f},
+    {-0.13f, -0.40f, -0.00f},
+    {-0.70f, -0.40f, -0.17f},
+    {-0.70f, -1.00f, -0.17f},
+    // ShortBox Bottom Face
+    {-0.53f, -1.00f, -0.75f},
+    {-0.70f, -1.00f, -0.17f},
+    {-0.13f, -1.00f, -0.00f},
+    {0.05f, -1.00f, -0.57f},
+    // TallBox Top Face
+    {0.53f, 0.20f, -0.09f},
+    {-0.04f, 0.20f, 0.09f},
+    {0.14f, 0.20f, 0.67f},
+    {0.71f, 0.20f, 0.49f},
+    // TallBox Left Face
+    {0.53f, -1.00f, -0.09f},
+    {0.53f, 0.20f, -0.09f},
+    {0.71f, 0.20f, 0.49f},
+    {0.71f, -1.00f, 0.49f},
+    // TallBox Front Face
+    {0.71f, -1.00f, 0.49f},
+    {0.71f, 0.20f, 0.49f},
+    {0.14f, 0.20f, 0.67f},
+    {0.14f, -1.00f, 0.67f},
+    // TallBox Right Face
+    {0.14f, -1.00f, 0.67f},
+    {0.14f, 0.20f, 0.67f},
+    {-0.04f, 0.20f, 0.09f},
+    {-0.04f, -1.00f, 0.09f},
+    // TallBox Back Face
+    {-0.04f, -1.00f, 0.09f},
+    {-0.04f, 0.20f, 0.09f},
+    {0.53f, 0.20f, -0.09f},
+    {0.53f, -1.00f, -0.09f},
+    // TallBox Bottom Face
+    {0.53f, -1.00f, -0.09f},
+    {-0.04f, -1.00f, 0.09f},
+    {0.14f, -1.00f, 0.67f},
+    {0.71f, -1.00f, 0.49f}};
+
+static std::vector<math::uint3> indices = {
+    {0, 1, 2}, // Floor
+    {0, 2, 3}, // Floor
+    {4, 5, 6}, // Ceiling
+    {4, 6, 7}, // Ceiling
+    {8, 9, 10}, // Backwall
+    {8, 10, 11}, // Backwall
+    {12, 13, 14}, // RightWall
+    {12, 14, 15}, // RightWall
+    {16, 17, 18}, // LeftWall
+    {16, 18, 19}, // LeftWall
+    {20, 22, 23}, // ShortBox Top Face
+    {20, 21, 22}, // ShortBox Top Face
+    {24, 25, 26}, // ShortBox Left Face
+    {24, 26, 27}, // ShortBox Left Face
+    {28, 29, 30}, // ShortBox Front Face
+    {28, 30, 31}, // ShortBox Front Face
+    {32, 33, 34}, // ShortBox Right Face
+    {32, 34, 35}, // ShortBox Right Face
+    {36, 37, 38}, // ShortBox Back Face
+    {36, 38, 39}, // ShortBox Back Face
+    {40, 41, 42}, // ShortBox Bottom Face
+    {40, 42, 43}, // ShortBox Bottom Face
+    {44, 45, 46}, // TallBox Top Face
+    {44, 46, 47}, // TallBox Top Face
+    {48, 49, 50}, // TallBox Left Face
+    {48, 50, 51}, // TallBox Left Face
+    {52, 53, 54}, // TallBox Front Face
+    {52, 54, 55}, // TallBox Front Face
+    {56, 57, 58}, // TallBox Right Face
+    {56, 58, 59}, // TallBox Right Face
+    {60, 61, 62}, // TallBox Back Face
+    {60, 62, 63}, // TallBox Back Face
+    {64, 65, 66}, // TallBox Bottom Face
+    {64, 66, 67}, // TallBox Bottom Face
+};
+
+static std::vector<math::float4> colors = {
+    // Floor
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // Ceiling
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // Backwall
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // RightWall
+    {0.140f, 0.450f, 0.091f, 1.0f},
+    {0.140f, 0.450f, 0.091f, 1.0f},
+    {0.140f, 0.450f, 0.091f, 1.0f},
+    {0.140f, 0.450f, 0.091f, 1.0f},
+    // LeftWall
+    {0.630f, 0.065f, 0.05f, 1.0f},
+    {0.630f, 0.065f, 0.05f, 1.0f},
+    {0.630f, 0.065f, 0.05f, 1.0f},
+    {0.630f, 0.065f, 0.05f, 1.0f},
+    // ShortBox Top Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // ShortBox Left Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // ShortBox Front Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // ShortBox Right Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // ShortBox Back Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // ShortBox Bottom Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // TallBox Top Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // TallBox Left Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // TallBox Front Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // TallBox Right Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // TallBox Back Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    // TallBox Bottom Face
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f},
+    {0.725f, 0.710f, 0.68f, 1.0f}};
+
+// CornelBox definitions //////////////////////////////////////////////////////
+
+anari::World CreateCornellBox(anari::Device d)
+{
+  auto m_world = anari::newObject<anari::World>(d);
+
+  auto geom = anari::newObject<anari::Geometry>(d, "triangle");
+
+  anari::setAndReleaseParameter(d,
+      geom,
+      "vertex.position",
+      anari::newArray1D(d, vertices.data(), vertices.size()));
+  anari::setAndReleaseParameter(d,
+      geom,
+      "vertex.color",
+      anari::newArray1D(d, colors.data(), colors.size()));
+  anari::setAndReleaseParameter(d,
+      geom,
+      "primitive.index",
+      anari::newArray1D(d, indices.data(), indices.size()));
+
+  anari::commitParameters(d, geom);
+
+  auto surface = anari::newObject<anari::Surface>(d);
+  anari::setAndReleaseParameter(d, surface, "geometry", geom);
+
+  auto mat = anari::newObject<anari::Material>(d, "matte");
+  anari::setParameter(d, mat, "color", "color");
+  anari::commitParameters(d, mat);
+  anari::setAndReleaseParameter(d, surface, "material", mat);
+
+  anari::commitParameters(d, surface);
+
+  anari::setAndReleaseParameter(
+      d, m_world, "surface", anari::newArray1D(d, &surface));
+  anari::release(d, surface);
+
+  anari::Light light;
+
+  // fix this up with new check
+  if (false/*anari::deviceImplements(d, "ANARI_KHR_AREA_LIGHTS")*/) {
+    light = anari::newObject<anari::Light>(d, "quad");
+    anari::setParameter(d, light, "color", math::float3(0.78f, 0.551f, 0.183f));
+    anari::setParameter(d, light, "intensity", 47.f);
+    anari::setParameter(d, light, "position", math::float3(-0.23f, 0.98f, -0.16f));
+    anari::setParameter(d, light, "edge1", math::float3(0.47f, 0.0f, 0.0f));
+    anari::setParameter(d, light, "edge2", math::float3(0.0f, 0.0f, 0.38f));
+  } else {
+    light = anari::newObject<anari::Light>(d, "directional");
+    anari::setParameter(d, light, "direction", math::float3(0.f, -0.5f, 1.f));
+  }
+
+  anari::commitParameters(d, light);
+
+  anari::setAndReleaseParameter(
+      d, m_world, "light", anari::newArray1D(d, &light));
+
+  anari::release(d, light);
+
+  anari::commitParameters(d, m_world);
+
+  return m_world;
+}
+
+}
+}
 
 namespace viewer {
 
@@ -114,29 +395,25 @@ struct Application : public anari_viewer::Application
     io.FontGlobalScale = 1.5f;
     io.IniFilename = nullptr;
 
+    ImGui::LoadIniSettingsFromMemory(g_defaultLayout);
+
     auto *viewport = new windows::Viewport(g_device, "Viewport");
     viewport->setManipulator(&m_state.manipulator);
 
     auto *leditor = new windows::LightsEditor(g_device);
 
-    auto *sselector = new windows::SceneSelector();
-    sselector->setCallback([=](const char *category, const char *scene) {
-      try {
-        auto s = anari::scenes::createScene(g_device, category, scene);
-        anari::scenes::commit(s);
-        auto w = anari::scenes::getWorld(s);
-        viewport->setWorld(w, true);
-        leditor->setWorlds({w});
-        sselector->setScene(s);
-      } catch (const std::runtime_error &e) {
-        printf("%s\n", e.what());
-      }
-    });
+    // Create a scene //
+
+    auto w = anari::scenes::CreateCornellBox(g_device);
+    viewport->setWorld(w, true);
+    leditor->setWorlds({w});
+    anari::release(g_device, w);
+
+    // Create Windows //
 
     anari_viewer::WindowArray windows;
     windows.emplace_back(viewport);
     windows.emplace_back(leditor);
-    windows.emplace_back(sselector);
 
     return windows;
   }
@@ -208,3 +485,41 @@ int main(int argc, char *argv[])
   app.run(1920, 1200, "ANARI Demo Viewer");
   return 0;
 }
+
+const char *g_defaultLayout =
+R"layout(
+[Window][MainDockSpace]
+Pos=0,25
+Size=1920,1174
+Collapsed=0
+
+[Window][Viewport]
+Pos=551,25
+Size=1369,1174
+Collapsed=0
+DockId=0x00000002,0
+
+[Window][Lights Editor]
+Pos=0,310
+Size=549,889
+Collapsed=0
+DockId=0x00000004,0
+
+[Window][Debug##Default]
+Pos=60,60
+Size=400,400
+Collapsed=0
+
+[Window][Scene]
+Pos=0,25
+Size=549,283
+Collapsed=0
+DockId=0x00000003,0
+
+[Docking][Data]
+DockSpace     ID=0x782A6D6B Window=0xDEDC5B90 Pos=0,25 Size=1920,1174 Split=X
+  DockNode    ID=0x00000001 Parent=0x782A6D6B SizeRef=549,1174 Split=Y Selected=0x5098EBE6
+    DockNode  ID=0x00000003 Parent=0x00000001 SizeRef=549,283 Selected=0xE192E354
+    DockNode  ID=0x00000004 Parent=0x00000001 SizeRef=549,889 Selected=0x5098EBE6
+  DockNode    ID=0x00000002 Parent=0x782A6D6B SizeRef=1369,1174 CentralNode=1 Selected=0x13926F0B
+)layout";
